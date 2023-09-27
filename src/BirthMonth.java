28c92682bb426ff8c687ee8b897efce19f3ed322
AbhiGone <giriabhinaya@gmail.com>
2023-09-27 09:27:52 -0400

    Birthmonth

--- src/BirthMonth.java
+++ src/BirthMonth.java
@@ -1,5 +1,17 @@
-public class Main {
+import java.util.Scanner;
+
+public class BirthMonth {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scan = new Scanner(System.in);
+        int birthMonth;
+        System.out.println("what is your birth month number (1-12)");
+        birthMonth = scan.nextInt();
+
+        if(birthMonth > 0 && birthMonth <= 12){
+            System.out.println("your birth month is " + birthMonth);
+        }else {
+            System.out.println("you have entered an inncorrect birth month \"" + birthMonth + "\"");
+        }
     }
 }
